74ab80bd59da157dea43b6650ff92a8a3c92552a
EngJao89 <eng_joao@outlook.com>
2025-02-18 17:24:52 -0300

    feat(agb11-interceptors): create interceptor in main api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { LogInterceptor } from './interceptors/log.interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalInterceptors(new LogInterceptor());
+
   await app.listen(3333);
 }
 bootstrap();
