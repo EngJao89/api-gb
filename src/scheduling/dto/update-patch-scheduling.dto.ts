560bdd926fabbad29909f3973387d25222852628
EngJao89 <eng_joao@outlook.com>
2025-09-15 22:07:03 -0300

    feat(agb60-validators-date): create validators of minimum date of scheduling

--- src/scheduling/dto/update-patch-scheduling.dto.ts
+++ src/scheduling/dto/update-patch-scheduling.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateSchedulingDTO } from "./create-scheduling.dto";
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateSchedulingDTO } from './create-scheduling.dto';
 
 export class UpdatePatchSchedulingDTO extends PartialType(CreateSchedulingDTO) {}
